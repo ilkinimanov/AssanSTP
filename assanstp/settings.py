98b46ec249c3ba6f5b8eb49a33573401e7558157
Ilkin Imanov <coderilkin.03@gmail.com>
2024-08-05 14:20:17 +0400

    Update ALLOWED_HOSTS to use environment variable in settings
    - Modified `ALLOWED_HOSTS` to be set from environment variable `ALLOWED_HOSTS` if `DEBUG` is `False`

--- assanstp/settings.py
+++ assanstp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('APP_DEBUG').lower() == 'true' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',') if not DEBUG else []
 
 
 # Application definition
