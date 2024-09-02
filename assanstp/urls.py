f84ee50e2a87674697223e52804e844d52cdf4ff
Ilkin Imanov <coderilkin.03@gmail.com>
2024-09-02 15:25:03 +0400

    Update Django project settings, URLs, and dependencies
    - Removed auto-generated comments in `settings.py` and `urls.py` for cleaner configuration.
    - Modified `ALLOWED_HOSTS` to allow all hosts by default.
    - Added `Task`, `Board`, and `corsheaders` apps to `INSTALLED_APPS`.
    - Configured CORS settings to allow all origins and added related middleware.
    - Extended JWT token lifetimes to one day.
    - Updated `urls.py` to include routes for `Task`, `User`, and `Board` APIs.
    - Enabled `APPEND_SLASH` to be `False` in settings.
    - Added new dependencies in `requirements.txt` for bidirectional dictionaries, WebSocket support, and CORS handling.

--- assanstp/urls.py
+++ assanstp/urls.py
@@ -1,35 +1,8 @@
-"""
-URL configuration for assanstp project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import include, path
-from rest_framework import routers
-from User import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/task', include('Task.urls')),
+    path('api/token', include('User.urls')),
+    path('api/board', include('Board.urls')),
 ]
